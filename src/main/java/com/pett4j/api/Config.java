d331686b805794057a1fec084543f97dadd998f8
pett4j <petterson.andrade@sensedia.com>
2017-04-24 03:58:42 -0300

    Simple cache

--- src/main/java/com/pett4j/api/Config.java
+++ src/main/java/com/pett4j/api/Config.java
@@ -3,15 +3,14 @@ package com.pett4j.api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.cache.RedisCacheManager;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
 
 import com.pett4j.api.filters.CachingPostFilter;
 import com.pett4j.api.filters.CachingPreFilter;
+
 
 @Configuration
 @EnableCaching
@@ -24,33 +23,8 @@ public class Config {
 	private CacheManager cacheManager;
 
 	@Bean
-	public JedisConnectionFactory redisConnectionFactory() {
-		JedisConnectionFactory redisConnectionFactory = new JedisConnectionFactory();
-
-		// Defaults
-		redisConnectionFactory.setHostName("172.17.0.2");
-		redisConnectionFactory.setPort(6379);
-		return redisConnectionFactory;
-	}
-
-    @Bean
-    public RedisTemplate<Object, Object> redisTemplate()
-    {
-        RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<Object, Object>();
-        redisTemplate.setConnectionFactory(redisConnectionFactory());
-        redisTemplate.setExposeConnection(true);
-        
-        return redisTemplate;
-    }
-    
-    @Bean
-    public RedisCacheManager cacheManager()
-    {
-        RedisCacheManager redisCacheManager = new RedisCacheManager(redisTemplate());
-        redisCacheManager.setTransactionAware(true);
-        redisCacheManager.setLoadRemoteCachesOnStartup(true);
-        redisCacheManager.setUsePrefix(true);
-        return redisCacheManager;
+    public CacheManager cacheManager() {
+        return new ConcurrentMapCacheManager(EIRCODE_CACHE);
     }
 	
 	@Bean
